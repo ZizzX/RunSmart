2c7bc347ae574098bf14a37c41143c04004ce89e
Aziz <zizzxiii@gmail.com>
2020-08-31 21:01:40 +0600

    add carousel

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-  $(".carousel-inner").slick({
+  $(".carousel__inner").slick({
+    speed: 1200,
+    // adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="img/icon/carousel/left-arrow.svg"></button>',
+      '<button type="button" class="slick-prev"><img src="img/icon/carousel/left.png"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="img/icon/carousel/right-arrow.svg"></button>',
+      '<button type="button" class="slick-next"><img src="img/icon/carousel/right.png"></button>',
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          dots: true,
+          arrows: false,
+          dotsClass: "slick-dots",
+        },
+      },
+    ],
+    autoplay: true,
+    autoplaySpeed: 2000,
   });
 });
