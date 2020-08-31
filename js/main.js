c746a22521ace1152ccfb22edcc77326fd3bdfad
Aziz <zizzxiii@gmail.com>
2020-08-31 19:12:34 +0600

    add carousel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-  $(".carousel-inner").slick();
+  $(".carousel-inner").slick({
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="img/icon/carousel/left-arrow.svg"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="img/icon/carousel/right-arrow.svg"></button>',
+  });
 });
