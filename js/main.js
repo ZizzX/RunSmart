b7c179db5b28ed44fa605d06baaeeb084f0f0ffc
Aziz <zizzxiii@gmail.com>
2020-09-01 16:46:37 +0600

    tiny slider add

--- js/main.js
+++ js/main.js
@@ -1,22 +1,39 @@
-$(document).ready(function () {
-  $(".carousel__inner").slick({
-    speed: 1200,
-    // adaptiveHeight: true,
-    prevArrow:
-      '<button type="button" class="slick-prev"><img src="img/icon/carousel/left.png"></button>',
-    nextArrow:
-      '<button type="button" class="slick-next"><img src="img/icon/carousel/right.png"></button>',
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          dots: true,
-          arrows: false,
-          dotsClass: "slick-dots",
-        },
-      },
-    ],
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+// $(document).ready(function () {
+//   $(".carousel__inner").slick({
+//     speed: 1200,
+//     // adaptiveHeight: true,
+//     prevArrow:
+//       '<button type="button" class="slick-prev"><img src="img/icon/carousel/left.png"></button>',
+//     nextArrow:
+//       '<button type="button" class="slick-next"><img src="img/icon/carousel/right.png"></button>',
+//     responsive: [
+//       {
+//         breakpoint: 992,
+//         settings: {
+//           dots: true,
+//           arrows: false,
+//           dotsClass: "slick-dots",
+//         },
+//       },
+//     ],
+//     autoplay: true,
+//     autoplaySpeed: 2000,
+//   });
+// });
+
+const slider = tns({
+  container: ".carousel__inner",
+  items: 1,
+  slideBy: "page",
+  autoplay: false,
+  controls: false,
+  nav: false,
+});
+
+document.querySelector(".prev").addEventListener("click", function () {
+  slider.goTo("prev");
+});
+
+document.querySelector(".next").addEventListener("click", function () {
+  slider.goTo("next");
 });
