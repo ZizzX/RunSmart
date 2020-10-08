eff16fc6a2d27d96a215fe0ca7b7e060ad99e385
Aziz <zizzxiii@gmail.com>
2020-10-08 20:25:55 +0600

    add smooth scroll & php mailer

--- js/main.js
+++ js/main.js
@@ -102,4 +102,35 @@ $(document).ready(function () {
   validForm("#consultation-form");
 
   $("input[name=phone]").mask("+7 (999) 999-99-99");
+
+  $("form").submit(function (e) {
+    e.preventDefault;
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize(),
+    }).done(function () {
+      $(this).find("input").val("");
+      $("#consultation, #order").fadeOut();
+      $(".overlay, #thanks").fadeIn("slow");
+
+      $("form").trigger("reset");
+    });
+    return false;
+  });
+
+  //scroll up & smooth scroll
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 1200) {
+      $(".scrollup").fadeIn();
+    } else {
+      $(".scrollup").fadeOut();
+    }
+  });
+
+  $("a[href^='#']").click(function () {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
 });
